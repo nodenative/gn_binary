7b1d2c4e5d4d86d5d3d93cd11897c7a60edddc18
Ion Lupascu <ionlupascu@gmail.com>
2018-08-26 18:27:36 +0100

    correct bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ function getGnHash()
   if [ "$OS_NAME" == "win" ]; then
     eval FILE_EXT=.exe
   fi
-  local hash=`curl https://chromium.googlesource.com/chromium/buildtools/+/master/$OS_NAME/gn$FILE_EXT.sha1 | grep -Po '<td class="FileContents-lineContents" id="1"><span class="pln">\K[A-Za-z0-9]+'`
+  local hash=`curl https://chromium.googlesource.com/chromium/buildtools/+/master/$OS_NAME/gn$FILE_EXT.sha1 | grep -Po '<td class="FileContents-lineContents" id="1"><span class="lit">\K[A-Za-z0-9]+'`
   echo $hash
 }
 
@@ -20,19 +20,20 @@ if [ ! -f gsutil/gsutil ]
     gsutil/gsutil --version
 fi
 
+echo "Creating directories..."
 mkdir -p mac
 mkdir -p linux
 mkdir -p win
 
 # https://chromium.googlesource.com/chromium/buildtools/+/master/win/gn.exe.sha1
-echo "win: $(getGnHash win)"
-gsutil/gsutil cp gs://chromium-gn/$(getGnHash win) win/gn.exe
+echo "copy win gn into win/gn.exe with version: $(getGnHash win)"
+./gsutil/gsutil cp gs://chromium-gn/$(getGnHash win) win/gn.exe
 
 # https://chromium.googlesource.com/chromium/buildtools/+/master/mac/gn.sha1
-echo "mac: $(getGnHash mac)"
-gsutil/gsutil cp gs://chromium-gn/$(getGnHash mac) mac/gn
+echo "copy mac gn into mac/gn with version: $(getGnHash mac)"
+./gsutil/gsutil cp gs://chromium-gn/$(getGnHash mac) mac/gn
 
 # https://chromium.googlesource.com/chromium/buildtools/+/master/linux64/gn.sha1 
-echo "linux64: $(getGnHash linux64)"
-gsutil/gsutil cp gs://chromium-gn/$(getGnHash linux64) linux/gn
+echo "copy linux64 gn into win/gn.exe with version: $(getGnHash linux64)"
+./gsutil/gsutil cp gs://chromium-gn/$(getGnHash linux64) linux/gn
 
